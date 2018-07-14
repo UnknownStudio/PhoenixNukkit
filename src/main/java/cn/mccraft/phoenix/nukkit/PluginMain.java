dafc7d56a22086556212e39cb064501b42538648
Trychen <trychen@live.com>
2018-07-14 18:37:57 +0800

    Inited project

---
+++ src/main/java/cn/mccraft/phoenix/nukkit/PluginMain.java
@@ -0,0 +1,10 @@
+package cn.mccraft.phoenix.nukkit;
+
+import cn.nukkit.plugin.PluginBase;
+
+public class PluginMain extends PluginBase {
+    @Override
+    public void onEnable() {
+        // where plugin start
+    }
+}
